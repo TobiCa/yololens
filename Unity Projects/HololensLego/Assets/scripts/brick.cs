998fed2248f57a17901ecb29770f23497477f69a
TorSalve <torsalve.dalsgaard@yahoo.de>
2017-05-07 13:29:53 +0200

    commit it all

---
+++ Unity Projects/HololensLego/Assets/scripts/brick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public int rotationSpeed = 30;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    Vector3 screenSpace;
+    Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenSpace = Camera.main.WorldToScreenPoint(transform.position);
+
+        var vec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(vec);
+
+    }
+
+    void OnMouseDrag()
+    {
+
+        var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+        var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+        transform.position = curPosition;
+    }
+}
