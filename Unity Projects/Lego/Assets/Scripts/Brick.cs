e3962ba63681b691adeb81d4d94e888c3d4cf45f
Tor-Salve Dalsgaard <torsalve.dalsgaard@yahoo.de>
2017-05-18 20:16:59 +0200

    some lego work and gitignore 2.0

--- Unity Projects/Lego/Assets/Scripts/Brick.cs
+++ Unity Projects/Lego/Assets/Scripts/Brick.cs
@@ -5,39 +5,107 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
-	public int rotation = 90;
-	public int id;
-	public int width;
-	public int hight;
+	[SerializeField] private int rotation = 90;
+    [SerializeField] private int id;
+    [SerializeField] private int width;
+    [SerializeField] private int hight;
 
-	private float stdSizeX = .8f;
+    private bool dragging = false;
+    private float distance;
+
+    private bool snapped = false;
+    private Vector3 lastPosition;
+    private Rigidbody rigidBody;
+
+    private float stdSizeX = .8f;
 	private float stdSizeY = 1.0f;
 	private float stdSizeZ = .8f;
 
+    private float small = 0.0001f;
+
+    [SerializeField] private GameObject board;
+
 	// Use this for initialization
 	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        lastPosition = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.rotation = Quaternion.Euler(-90, rotation, 0);
+    
+    // Update is called once per frame
+    void Update () {
+        transform.rotation = Quaternion.Euler(-90, rotation, 0);
 
-		var currentPos = transform.position;
-		transform.position = new Vector3(	
-										(float)Math.Round(currentPos.x),
-		                             	(float)Math.Round(currentPos.y),
-		                             	(float)Math.Round(currentPos.z)
-		                             	);
+        var currentPos = transform.position;
 
-	}
+        float x = nearestMultiple(currentPos.x, stdSizeX + small);
+        float z = nearestMultiple(currentPos.z, stdSizeZ + small);
+        transform.position = new Vector3(	
+                                            x,
+                                            currentPos.y,
+                                            z
+                                        );
+        if (dragging) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            rayPoint.x = Limit(rayPoint.x, board.GetComponent<Collider>().bounds.size.x / 2 - board.transform.position.x, board.GetComponent<Collider>().bounds.size.x / 2 + board.transform.position.x);
+            rayPoint.y = Limit(rayPoint.y, board.GetComponent<Collider>().bounds.size.y, 100);
+            rayPoint.z = Limit(rayPoint.z, board.GetComponent<Collider>().bounds.size.z / 2 - board.transform.position.z, board.GetComponent<Collider>().bounds.size.z / 2 + board.transform.position.z);
+            transform.position = rayPoint;
+            rigidBody.useGravity = true;
+            snapped = false;
+        }
 
-	void OnCollisionEnter(Collision col) {
-		if (col.gameObject.name == "4x2") {
-			col.gameObject.transform.position = new Vector3(
-										(float)Math.Round(transform.position.x + stdSizeX * 2),
-		                             	(float)Math.Round(transform.position.y + stdSizeY * 2),
-		                             	(float)Math.Round(transform.position.z + stdSizeZ * 2)
-		                             	);
-		}
-	}
+        if (dragging && snapped)
+        {
+            transform.position = lastPosition;
+        }
+
+        lastPosition = transform.position;
+
+        if (snapped)
+        {
+            rigidBody.useGravity = true;
+            transform.position = lastPosition;
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "4x2") {
+           /* col.gameObject.transform.position = new Vector3(
+                                                    (float)Math.Round(transform.position.x),
+                                                    (float)Math.Round(transform.position.y),
+                                                    (float)Math.Round(transform.position.z)
+                                                );*/
+            rigidBody.useGravity = false;
+            snapped = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    private float nearestMultiple(float value, float factor)
+    {
+        return (float)Math.Round(
+                     (value / (double)factor),
+                     MidpointRounding.AwayFromZero
+                 ) * factor;
+
+    }
+
+    private static float Limit(
+        float value, float inclusiveMinimum, float inclusiveMaximum)
+    {
+        if (value < inclusiveMinimum) { return inclusiveMinimum; }
+        if (value > inclusiveMaximum) { return inclusiveMaximum; }
+        return value;
+    }
 }
